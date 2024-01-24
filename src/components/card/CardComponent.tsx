3c44df6f6ab943d690f75671e7dea1486436be87
mercurystudio99 <mercurystudio99@gmail.com>
2024-01-23 23:35:38 -0800

    add icon

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
+import Box from '@mui/material/Box';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+
 import "./CardComponent.css";
 import { card } from "../../imageRouter";
 class CardComponent extends React.Component {
   render() {
     return (
-      <Card sx={{ maxWidth: 345 }}>
+      <Card>
       <CardMedia
         component="img"
         alt="image"
@@ -21,15 +22,21 @@ class CardComponent extends React.Component {
         <Typography gutterBottom variant="h5" component="div">
           Lizard
         </Typography>
+        <Typography gutterBottom component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Conditions: Consistency, temperature, bottom,
+        </Typography>
         <Typography variant="body2" color="text.secondary">
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+        <AccessAlarm></AccessAlarm>
+        <ThreeDRotation></ThreeDRotation>
+      </Box>
     </Card>
       );
   }
