4a3498a1dcf246a0db84cf79024246b23ec230f7
mercurystudio99 <mercurystudio99@gmail.com>
2024-01-23 20:18:07 -0800

    add card component

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,20 +1,13 @@
 import * as React from "react";
 import "../../assets/styles/SharedStyles.css";
+import CardComponent from "../../components/card/CardComponent";
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="page">
         <div className="pageTitle">Stack Exchange Search</div>
-        <div className="mainCard">
-          <p>This will search Stack Exchange questions for given title text.</p>
-          <p>
-            Enter your search text and click the icon or press the Enter key. A
-            list of matching questions will be shown.
-          </p>
-          <p>After that, you can click on a question to see the answers.</p>
-          <h4>By JC Lango</h4>
-        </div>
+        <CardComponent></CardComponent>
       </div>
     );
   }
