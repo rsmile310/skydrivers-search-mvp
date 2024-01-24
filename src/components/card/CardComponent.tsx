f061b5feaa1f71e279298ad80b577bff8c46ca48
mercurystudio99 <mercurystudio99@gmail.com>
2024-01-23 23:56:09 -0800

    update card component

--- src/components/card/CardComponent.tsx
+++ src/components/card/CardComponent.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
@@ -8,6 +13,15 @@ import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
 
 import "./CardComponent.css";
 import { card } from "../../imageRouter";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 class CardComponent extends React.Component {
   render() {
     return (
@@ -32,11 +46,41 @@ class CardComponent extends React.Component {
           Lizards are a widespread group of squamate reptiles, with over 6,000
           species, ranging across all continents except Antarctica
         </Typography>
+        <Divider light />
+        <Stack direction="row" alignItems="center">
+          <AccessAlarm></AccessAlarm>
+          <Typography gutterBottom variant="h5" component="div">
+            EZE &lt; &gt; LYO
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            round trip flight
+          </Typography>
+        </Stack>
+        <Divider light />
       </CardContent>
-      <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-        <AccessAlarm></AccessAlarm>
-        <ThreeDRotation></ThreeDRotation>
-      </Box>
+      <Grid container spacing={3}>
+        <Grid xs>
+          <Item>
+            <Typography>Best Flight</Typography>
+            <Typography>1300 &euro;</Typography>
+            <Typography>12h 20min</Typography>
+          </Item>
+        </Grid>
+        <Grid xs>
+          <Item>
+            <Typography>Best Flight</Typography>
+            <Typography>1300 &euro;</Typography>
+            <Typography>12h 20min</Typography>
+          </Item>
+        </Grid>
+        <Grid xs>
+          <Item>
+            <Typography>Best Flight</Typography>
+            <Typography>1300 &euro;</Typography>
+            <Typography>12h 20min</Typography>
+          </Item>
+        </Grid>
+      </Grid>
     </Card>
       );
   }
